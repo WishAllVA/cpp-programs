8e5f13645d32285d02aa9a6f7da1874d731d1291
Vishal Arora <aroravishal629@gmail.com>
2021-07-08 23:44:02 +0530

    Added Pascal triangle program

---
+++ Pascal Triangle.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n;
+	cin>>n;
+	int arr[n][n];
+	for(int i=0;i<=n-1;i++)
+	{
+		for(int j=0;j<=i;j++)
+		{
+			if(j==0 || j==i)
+			{
+				arr[i][j]=1;
+			}
+			else
+			{
+				arr[i][j]=arr[i-1][j-1]+arr[i-1][j];
+			}
+		}
+	}
+	for(int i=0;i<=n-1;i++)
+	{
+		for(int j=0;j<=i;j++)
+		{
+			cout<<arr[i][j]<<" ";
+		}
+		cout<<"\n";
+	}
+}
