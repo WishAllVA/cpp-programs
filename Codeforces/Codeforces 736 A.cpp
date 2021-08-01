2155526b39ac062718f40c9c91b334712f156696
Vishal Arora <aroravishal629@gmail.com>
2021-08-01 23:01:07 +0530

    Added solution to Codeforces 736 A

---
+++ Codeforces/Codeforces 736 A.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef unsigned long long ULL;
+typedef long long LL;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--) {
+		LL p;
+		cin>>p;
+		p--;
+		cout<<2<<" "<<p<<endl;
+	}
+	return 0;
+}
